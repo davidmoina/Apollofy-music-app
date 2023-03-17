2f3f67aeaed5d615435ed10783fe4bbfceda4472
davidmoina <luisdavidmoina@gmail.com>
2023-03-17 09:26:58 +0100

    added tailwind

---
+++ src/components/asideMenu/Aside.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './aside.module.scss'
+
+const Aside = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Aside
