b16893e964a910d20c9c8bd873a824376e14b536
luisao8 <luisalarconriva@gmail.com>
2023-03-20 12:30:02 +0100

    Mobile Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import './sass/index.scss'
+import React, { FC } from 'react';
+import './sass/index.scss';
+import MobileNavBar from './components/MobileNavBar/MobileNavBar'; // Corrected import statement
 
-function App() {
-
+const App: FC = () => {
   return (
     <>
-    <div className="App">
-      <h1>home page</h1>
-    </div>
-    <div className='tests'>
-      <p>Text colors</p>
-    </div>
+      <div className="App">
+        <h1>home page</h1>
+      </div>
+      <div className="tests">
+        <p>Text colors</p>
+      </div>
+      <MobileNavBar />
     </>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/MobileNavBar/MobileNavBar.tsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+import { MdHomeFilled } from 'react-icons/md';
+import { BiSearch } from 'react-icons/bi';
+import { BiLibrary } from 'react-icons/bi';
+import { useMediaQuery } from '@react-hook/media-query';
+import { FC } from 'react';
+
+
+interface Props {
+  icon: React.ReactNode;
+  text: string;
+}
+
+const MobileNavButton = ({ icon, text }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(prevState => !prevState);
+  };
+
+  const handleTransitionEnd = () => {
+    if (isExpanded) {
+      setIsExpanded(false);
+    }
+  };
+
+  return (
+    <div
+      className={`text-center flex flex-col items-center ${
+        isExpanded
+          ? 'scale-90 transition duration-100 linear'
+          : 'scale-100'
+      }`}
+      onClick={toggleExpanded}
+      onTransitionEnd={handleTransitionEnd}
+    >
+      <div className="sidebar-icon">{icon}</div>
+      <p className="text-white text-sm">{text}</p>
+    </div>
+  );
+};
+
+const MobileNavBar: FC = () => {
+  const mediaQuery = useMediaQuery('(max-width: 767px)');
+  const [isPhoneScreen, setIsPhoneScreen] = useState<boolean>(mediaQuery);
+
+  useEffect(() => {
+    setIsPhoneScreen(mediaQuery);
+  }, [mediaQuery]);
+
+  if (!isPhoneScreen) {
+    return null;
+  }
+
+  
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 h-20 bg-black flex justify-around items-center shadow-lg">
+      <MobileNavButton
+        icon={<MdHomeFilled size="30" style={{ color: 'white' }} />}
+        text="Home"
+      />
+      <MobileNavButton
+        icon={<BiSearch size="30" style={{ color: 'white' }} />}
+        text="Search"
+      />
+      <MobileNavButton
+        icon={<BiLibrary size="30" style={{ color: 'white' }} />}
+        text="Playlists"
+      />
+    </nav>
+  );
+};
+
+export default MobileNavBar;
