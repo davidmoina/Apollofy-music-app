1cf0aaeb04cfdce39dea067b83ed4a70dc2f1042
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-03-24 15:02:53 +0100

    Feat: Fixed header

--- src/views/PlaylistsView/PlaylistsView.tsx
+++ src/views/PlaylistsView/PlaylistsView.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import { MusicRow } from '../../components/musicRow/MusicRow'
 import { SongListContainer } from '../../containers/songListContainer/SongListContainer';
 import  PlaylistHeader  from '../../components/playlistHeader/PlaylistHeader';
 import PlaylistInfoBar from '../../components/infoBarPlaylists/InfoBarPlaylists';
-import styles from '../../infoBarPlaylists.module.scss'; 
+
+export const samplePlaylists = 
+{
+  "id": 1,
+  "name": "Road trip! All the masters I need!",
+  "isFollowed": true,
+  "thumbnail": "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644625060/tracks-thumbnails-dev/car_yx6ld1.jpg",
+  "description": "Get your mic on with this beats. You are going to sing all the way down",
+  "publicAccessible": true,
+  "primaryColor": "#fbdc00"
+}
 
 export const PlaylistsView = () => {
 
-  const samplePlaylists = 
-  {
-    "id": 1,
-    "name": "Road trip! All the masters I need!",
-    "isFollowed": true,
-    "thumbnail": "https://res.cloudinary.com/dmkdsujzh/image/upload/v1644625060/tracks-thumbnails-dev/car_yx6ld1.jpg",
-    "description": "Get your mic on with this beats. You are going to sing all the way down",
-    "publicAccessible": true,
-    "primaryColor": "#fbdc00"
-  }
+
   return (
     <div>
       <PlaylistHeader {...samplePlaylists}/>
