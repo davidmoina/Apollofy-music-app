e924dced00de78354db4a338a59d8ce880b73874
davidmoina <luisdavidmoina@gmail.com>
2023-05-16 14:11:38 +0200

    Feat: play playlists

--- src/components/playlistHeader/PlaylistHeader.tsx
+++ src/components/playlistHeader/PlaylistHeader.tsx
@@ -1,5 +1,8 @@
+import { useContext, useState } from 'react';
 import { Playlist } from '../../interfaces/playlist';
 import styles from './playlistHeader.module.scss';
+import { MdPlayCircleFilled } from 'react-icons/md';
+import { PlayerContext } from '../../context/PlayerContext/PlayerContext';
 
 interface Props {
 	data: Playlist<string> | null;
@@ -7,14 +10,36 @@ interface Props {
 
 function PlaylistHeader({ data }: Props) {
 	console.log(data?.publicAccessible);
+	const [showPlay, setShowPlay] = useState(false);
+
+	const { songsSet, setCurrent } = useContext(PlayerContext);
+
+	const handleClickSong = () => {
+		songsSet(data!.tracks);
+		setCurrent(0, data?.tracks[0]);
+
+		// togglePlaying();
+	};
 
 	return (
 		<div className={styles.playlistHeader}>
-			<img
-				className={styles.playlistImage}
-				src={data?.thumbnail}
-				alt={data?.name}
-			/>
+			<div
+				className='relative'
+				onMouseEnter={() => setShowPlay(true)}
+				onMouseLeave={() => setShowPlay(false)}
+			>
+				<img
+					className={styles.playlistImage}
+					src={data?.thumbnail}
+					alt={data?.name}
+				/>
+				<MdPlayCircleFilled
+					onClick={handleClickSong}
+					className={`cursor-pointer absolute text-5xl rounded-lg z-50 bottom-3 right-0 text-[#ffff66] ${
+						!showPlay ? 'opacity-0' : 'opacity-100 bottom-3 right-3'
+					} transition-all duration-200`}
+				/>
+			</div>
 			<div className={styles.playlistInfo}>
 				<p className={styles.publicPlaylist}>
 					{data?.publicAccessible ? 'Public Playlist' : 'Private playlist'}
